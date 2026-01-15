024ee9a72747f5aa0fe368a4e208a820560aee4a
tracey <tracebee23@gmail.com>
2026-01-15 16:07:01 -0600

    parameter properties shortcuts

---
+++ TYPESCRIPT-TRAINING/04-classes/04-parameter-properties/Customer.ts
@@ -0,0 +1,30 @@
+class Customer {
+
+
+    constructor(private _firstName: string, private _lastName: string) {
+    }
+
+    public get firstName(): string {
+        return this._firstName;
+    }
+
+    public set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+
+}
+
+
+//create instance
+let myCustomer = new Customer("John", "Doe");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
