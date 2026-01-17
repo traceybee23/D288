2998d470ca8e9ce06523ebae1895f9f23991238f
tracey <tracebee23@gmail.com>
2026-01-17 13:53:03 -0600

    looping over sales people list to populate template

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-sales-person-list',
-  imports: [],
   templateUrl: './sales-person-list.html',
   styleUrl: './sales-person-list.css',
+  imports: [CommonModule]
 })
-export class SalesPersonList {
+
+export class SalesPersonList implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@example.com', 50000),
+    new SalesPerson('John', 'Doe', 'john.doe@example.com', 40000),
+    new SalesPerson('Claire', 'Murphy', 'claire.murphy@example.com', 90000),
+    new SalesPerson('Mai', 'Truong', 'mai.truong@example.com', 60000)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 
 }
